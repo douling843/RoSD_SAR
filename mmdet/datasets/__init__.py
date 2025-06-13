85342a7729eda4ead5793066c8d33348e5f7c8aa
guo <48042521+douling843@users.noreply.github.com>
2025-06-13 20:43:01 +0800

    Add files via upload

---
+++ mmdet/datasets/__init__.py
@@ -0,0 +1,20 @@
+from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
+from .custom import CustomDataset
+from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
+                               RepeatDataset)
+from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
+from .utils import replace_ImageToTensor
+from .xml_style import XMLDataset
+
+from .voc07 import VOCDataset
+from .coco import CocoDataset
+from .ssdd import SsddDataset
+from .hrsid import HrsidDataset
+
+__all__ = [
+    'CustomDataset', 'XMLDataset', 'CocoDataset',
+    'VOCDataset', 'GroupSampler', 'DistributedGroupSampler',
+    'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
+    'ClassBalancedDataset', 'DATASETS', 'PIPELINES',
+    'build_dataset', 'replace_ImageToTensor', 'VOCDataset', 'CocoDataset', 'SsddDataset', 'HrsidDataset'
+]
